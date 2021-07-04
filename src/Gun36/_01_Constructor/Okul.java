4139bab7e97c0c879167d52aafad081538877f6e
Glcn Kskl <86896898+gkisakol1@users.noreply.github.com>
2021-07-04 19:39:01 +0200

    Update Okul.java

--- src/Gun36/_01_Constructor/Okul.java
+++ src/Gun36/_01_Constructor/Okul.java
@@ -10,23 +10,23 @@ public class Okul {
         // 1.Yöntem Nesne oluşturmak
         Student ogr1=new Student();
         ogr1.id = 1;
-        ogr1.name="Sevim";
-        ogr1.surName="Yılmaz";
+        ogr1.name="Gülcan";
+        ogr1.surName="Bergstein";
         ogr1.classroom=9;
         System.out.println("ogr1.name = " + ogr1.name);
 
         //2.Yöntem Nesne oluşturma
-        Student ogr2 = Student.createStudent(2,"ilhan", "Demir", 8);
+        Student ogr2 = Student.createStudent(2,"Elif", "Grifflinberg", 8);
         System.out.println("ogr2.name = " + ogr2.name);
 
         //3.Yöntem yeni!!!!
-        Student ogr3= new Student(3, "mehmet", "deniz", 4); // tanımlama anında çalışan metodlar var aslında
+        Student ogr3= new Student(3, "Bige", "Denizhan", 4); // tanımlama anında çalışan metodlar var aslında
                                      // ama sen kullanmadığın için bende göstermiyorum.
 
         System.out.println("ogr3.name = " + ogr3.name);
 
-        Student ogr4= new Student(3, "Ragıp", "Bayrak");
-        Student ogr5= new Student(3, "Ragıp");
+        Student ogr4= new Student(3, "Buket", "Denizhan");
+        Student ogr5= new Student(3, "Cayda");
         Student ogr6= new Student(3);
 
 
