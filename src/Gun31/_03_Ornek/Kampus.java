aedb69fc09ba672860f4f1224b1a767f80612d4b
Glcn Kskl <86896898+gkisakol1@users.noreply.github.com>
2021-07-04 19:31:11 +0200

    Update Kampus.java

--- src/Gun31/_03_Ornek/Kampus.java
+++ src/Gun31/_03_Ornek/Kampus.java
@@ -6,18 +6,18 @@ public class Kampus {
         // Okul ve Ogrenci sınıfları olan bir yapı tasarlayınız.
         Ogrenci ogrenci1 = new Ogrenci();
         ogrenci1.numara =1;
-        ogrenci1.Ad = "Mesut";
-        ogrenci1.Soyad ="Demir";
+        ogrenci1.Ad = "Zeynep";
+        ogrenci1.Soyad ="Grifflinberg";
 
         Ogrenci ogrenci2 = new Ogrenci();
         ogrenci2.numara=2;
         ogrenci2.Ad="Gülcan";
-        ogrenci2.Soyad="Bayrak";
+        ogrenci2.Soyad="Grifflinberg";
 
         Okul okul1=new Okul();
-        okul1.adi="Cumhuriyet";
-        okul1.adres="Merkez Mah.";
-        okul1.muduru="Ragıp";
+        okul1.adi="Costar";
+        okul1.adres="Karl Max str 3.";
+        okul1.muduru="Elif";
         okul1.ucreti=20000;
 
         ogrenci1.Okulu = okul1;  //Okul ekleme : 1.Yöntem
@@ -27,7 +27,7 @@ public class Kampus {
         // öğrenci1 i yazdırınız bütün bilgilerini
 
         ogrenci2.Okulu = new Okul(); //Okul ekleme : 2.Yöntem
-        ogrenci2.Okulu.muduru = "Hilal";
+        ogrenci2.Okulu.muduru = "Bilge";
         ogrenci2.Okulu.ucreti = 30000;
     }
 
