2dca88b8d83c0bd543ce0357eb3f6ad1cf337752
Glcn Kskl <86896898+gkisakol1@users.noreply.github.com>
2021-07-04 19:32:37 +0200

    Update _02_Personel.java

--- src/Gun31/_02_Personel.java
+++ src/Gun31/_02_Personel.java
@@ -10,8 +10,8 @@ public class _02_Personel {
            _02_Personel mudur=new _02_Personel();
 
            mudur.id=1;
-           mudur.name="Ayşe";
-           mudur.surName = "Yılmaz";
+           mudur.name="Elif";
+           mudur.surName = "Grifflinberg";
 
         System.out.println("mudur.id = " + mudur.id);
         System.out.println("mudur.name = " + mudur.name);
