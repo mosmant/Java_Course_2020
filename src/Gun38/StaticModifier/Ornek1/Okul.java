dc5b41e080598e2fc39f2afd6c28fd79f169531f
Glcn Kskl <86896898+gkisakol1@users.noreply.github.com>
2021-07-04 19:49:28 +0200

    Update Okul.java

--- src/Gun38/StaticModifier/Ornek1/Okul.java
+++ src/Gun38/StaticModifier/Ornek1/Okul.java
@@ -3,12 +3,12 @@ package Gun38.StaticModifier.Ornek1;
 public class Okul {
 
     public static void main(String[] args) {
-        Ogrenci ogr1=new Ogrenci("Ali","Yılmaz");
-        Ogrenci ogr2=new Ogrenci("Ayşe","Demir");
+        Ogrenci ogr1=new Ogrenci("Bige","Schwebinghaus");
+        Ogrenci ogr2=new Ogrenci("Gülcan","Grifflinberg");
 //        ...
 //        ...
 //        ...
-        Ogrenci ogr500=new Ogrenci("Hilal","Bayrak");
+        Ogrenci ogr500=new Ogrenci("Elif","Grifflinberg");
 
         System.out.println("ogr1 = " + ogr1);
         System.out.println("ogr2 = " + ogr2);
@@ -18,6 +18,6 @@ public class Okul {
 }
 
 
-//ogr1 = Ogrenci{ad='Ali', soyad='Yılmaz', okulAd='Yıldırım Lisesi'}
-//ogr2 = Ogrenci{ad='Ayşe', soyad='Demir', okulAd='Yıldırım Lisesi'}
-//ogr500 = Ogrenci{ad='Hilal', soyad='Bayrak', okulAd='Yıldırım Lisesi'}
+//ogr1 = Ogrenci{ad='Bige', soyad='Schwebinghaus', okulAd='Brüder Grimm Schule'}
+//ogr2 = Ogrenci{ad='Gülcan', soyad='Grifflinberg', okulAd='Brüder Grimm Schule'}
+//ogr500 = Ogrenci{ad='Elif', soyad='Grifflinberg', okulAd='Brüder Grimm Schule'}
