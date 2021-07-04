e7358d49621bc35a60d6f3f4c911ca5981b8d159
Glcn Kskl <86896898+gkisakol1@users.noreply.github.com>
2021-07-04 18:43:42 +0200

    Update _12_Soru.java

--- src/Gun08/_12_Soru.java
+++ src/Gun08/_12_Soru.java
@@ -6,13 +6,13 @@ public class _12_Soru {
     public static void main(String[] args) {
         // Kullanıcıdan tek okumada alacağınız 2 kelimelik ad ve soyadı ekrana
         // sadece isim başHarf.soysimBasharf. şeklinde yazdırınız.
-        // yani Ahmet Yılmaz -> A.Y. olarak yazdırınız.
+        // yani Gülcan Elif -> G.E. olarak yazdırınız.
 
         Scanner oku=new Scanner(System.in);
         System.out.print("Ad ve Soyadınız = ");
-        String tamAd=oku.nextLine(); // Ahmet Yılmaz
+        String tamAd=oku.nextLine(); // Gülcan Elif
 
-        char adBasHarf= tamAd.charAt(0); // A
+        char adBasHarf= tamAd.charAt(0); // G
         int boslukIndex = tamAd.indexOf(" ");
         char soyadIlkHarf = tamAd.charAt(boslukIndex+1);
 
